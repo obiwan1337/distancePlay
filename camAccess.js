b274f73d3ae7e879dd7dede4f4af7e91a21ec58c
obiwan1337 <43846856+obiwan1337@users.noreply.github.com>
2020-12-02 21:58:34 +0000

    Add files via upload
    some changes to the css.
    removed old code of the ts. cleaned up console.logs

--- camAccess.js
+++ camAccess.js
@@ -3,23 +3,20 @@ let videoTrack;
 let video = document.querySelector('#myOwnVideo');
 let ownVideoStream;
 const constraints = {
-    video: true, frameRate: { ideal: 10, max: 15 },
-    audio: false,
+    video: true, audio: false,
 };
 function init() {
     //create start and stop button
-    let buttonDiv = document.getElementById("button_Div");
-    console.log(buttonDiv + "see the div");
     let addButton = document.getElementById("add");
     let remButton = document.getElementById("stop");
     addButton.addEventListener("click", AddCamera);
     remButton.addEventListener("click", stopCamera);
-    console.log(navigator.mediaDevices.getUserMedia(constraints));
-    console.log("video" + video + "ownmediastream " + ownVideoStream);
+    console.log("init done");
 }
 function AddCamera() {
+    console.log("starting add");
     if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia(constraints)
+        navigator.mediaDevices.getUserMedia({ video: true })
             .then(function (stream) {
             video.srcObject = stream;
         })
@@ -27,23 +24,6 @@ function AddCamera() {
             console.log("Something went wrong!");
         });
     }
-    //const fetchOwnCam = async () => {
-    //    try {
-    //    navigator.mediaDevices.getUserMedia(constraints)
-    //        .then(
-    //            function (stream) {
-    //                //ownVideoStream.srcObject = mediaStream;
-    //
-    //                video.srcObject = stream;
-    //            }
-    //        )
-    //    }
-    //    catch (error) {
-    //    console.log("this was an error " + error)
-    //    }
-    //}
-    //console.log(video);
-    //video = ownVideoStream.srcObject;
 }
 function stopCamera(e) {
     let stream = video.srcObject;
--- camAccess.ts
+++ camAccess.ts
@@ -4,32 +4,27 @@ let videoTrack: HTMLMediaElement;
 let video = <HTMLVideoElement>document.querySelector('#myOwnVideo');
 let ownVideoStream;
 const constraints = {
-    video: true, frameRate: { ideal: 10, max: 15 },
-    audio: false,
+    video: true, audio: false,
 }
 
 
 function init(): void {
 
     //create start and stop button
-    let buttonDiv: HTMLElement = document.getElementById("button_Div");
-    console.log(buttonDiv + "see the div");
+
     let addButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("add");
     let remButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("stop");
 
     addButton.addEventListener("click", AddCamera);
     remButton.addEventListener("click", stopCamera);
 
-    console.log(navigator.mediaDevices.getUserMedia(constraints));
-    console.log("video" + video + "ownmediastream " + ownVideoStream);
-   
-
+    console.log("init done");
 }
 
 function AddCamera(): void {
-
+    console.log("starting add");
     if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia(constraints)
+        navigator.mediaDevices.getUserMedia({ video: true })
             .then(function (stream) {
                 video.srcObject = stream;
             })
@@ -37,26 +32,6 @@ function AddCamera(): void {
                 console.log("Something went wrong!");
             });
     }
-    //const fetchOwnCam = async () => {
-    //    try {
-    //    navigator.mediaDevices.getUserMedia(constraints)
-    //        .then(
-    //            function (stream) {
-    //                //ownVideoStream.srcObject = mediaStream;
-    //
-    //                video.srcObject = stream;
-    //            }
-    //        )
-    //    }
-    //    catch (error) {
-    //    console.log("this was an error " + error)
-    //    }
-    //}
-    //console.log(video);
-
-
-    //video = ownVideoStream.srcObject;
-
 }
 function stopCamera(e) {
     let stream: MediaStream = <MediaStream>video.srcObject;
