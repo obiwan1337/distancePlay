a81d781d509fa3b93536fdbce9fbaeb04a27ea99
obiwan1337 <43846856+obiwan1337@users.noreply.github.com>
2020-12-03 21:09:00 +0000

    Add files via upload
    inlne script was working. outsourced to JS file

--- camAccess.js
+++ camAccess.js
@@ -1,36 +1,11 @@
-document.addEventListener('DOMContentLoaded', init);
-let videoTrack;
-let video = document.querySelector('#myOwnVideo');
-let ownVideoStream;
-const constraints = {
-    video: true, audio: false,
-};
-function init() {
-    //create start and stop button
-    let addButton = document.getElementById("add");
-    let remButton = document.getElementById("stop");
-    addButton.addEventListener("click", AddCamera);
-    remButton.addEventListener("click", stopCamera);
-    console.log("init done");
-}
-function AddCamera() {
-    console.log("starting add");
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(function (stream) {
-            video.srcObject = stream;
-        })
-            .catch(function (err0r) {
-            console.log("Something went wrong!");
-        });
-    }
-}
-function stopCamera(e) {
-    let stream = video.srcObject;
-    let tracks = stream.getVideoTracks();
-    for (let i = 0; i < tracks.length; i++) {
-        tracks[0].stop();
-    }
-    video.srcObject = null;
+let video = document.querySelector("#videoElement");
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+        video.srcObject = stream;
+    })
+        .catch(function (err0r) {
+        console.log("Something went wrong!");
+    });
 }
 //# sourceMappingURL=camAccess.js.map
