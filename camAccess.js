527ecb5511aa6c4bdb9bc617cafc42611b838f9c
obiwan1337 <43846856+obiwan1337@users.noreply.github.com>
2020-12-03 21:14:42 +0000

    Add files via upload
    added DOMcontent loaded and made the code into an init function. eternal script seems to not work.

--- camAccess.js
+++ camAccess.js
@@ -1,11 +1,14 @@
-let video = document.querySelector("#videoElement");
-if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function (stream) {
-        video.srcObject = stream;
-    })
-        .catch(function (err0r) {
-        console.log("Something went wrong!");
-    });
+document.addEventListener('DOMContentLoaded', init);
+function init() {
+    let video = document.querySelector("#videoElement");
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+            video.srcObject = stream;
+        })
+            .catch(function (err0r) {
+            console.log("Something went wrong!");
+        });
+    }
 }
 //# sourceMappingURL=camAccess.js.map
