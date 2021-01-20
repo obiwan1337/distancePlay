622bfff189cb41ebc4c0b17886343f34f43391f5
obiwan1337 <43846856+obiwan1337@users.noreply.github.com>
2021-01-20 11:03:29 +0000

    Add files via upload

--- scripts/camAccess.ts
+++ scripts/camAccess.ts
@@ -1,9 +1,10 @@
+
 document.addEventListener('DOMContentLoaded', init);
-let videostream;
-let image;
-let canvas;
-let context;
-let ownCardList = [];
+let videostream: HTMLVideoElement;
+let image: HTMLImageElement;
+let canvas: HTMLCanvasElement;
+let context: CanvasRenderingContext2D;
+let ownCardList: { id: number, cardPictureLink: string, cardText: string }[] = [];
 let constraints = {
     video: {
         cursor: "never",
@@ -11,52 +12,65 @@ let constraints = {
         height: 720,
         frameRate: 15,
     }
-};
-function init() {
-    let addButton = document.getElementById("add");
-    let remButton = document.getElementById("stop");
+
+}
+function init(): void {
+    
+    let addButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("add");
+    let remButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("stop");
     addButton.addEventListener("click", addCam);
     remButton.addEventListener("click", stopCamera);
     videostream = document.querySelector("#ownVideoElement");
     videostream.addEventListener("click", takeCardSC);
     image = document.querySelector("#shotOfCard");
     canvas = document.querySelector("#convertCanvas");
+
     context = canvas.getContext('2d');
+
 }
+
+
 function addCam() {
     if (navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia(constraints)
             .then(function (stream) {
-            videostream.srcObject = stream;
-            canvas.style.width = videostream.width.toString() + "px";
-            canvas.style.height = videostream.height.toString() + "px";
-        })
+                videostream.srcObject = stream;
+                canvas.style.width = videostream.width.toString() + "px";
+                canvas.style.height = videostream.height.toString() + "px";
+            }
+
+            )
             .catch(function (err0r) {
-            console.log("Something went wrong!");
-        });
+                console.log("Something went wrong!");
+            });
     }
 }
-function stopCamera() {
-    let stream = videostream.srcObject;
+function stopCamera(): void {
+    let stream: MediaStream = <MediaStream>videostream.srcObject;
     let tracks = stream.getVideoTracks();
+
     for (let i = 0; i < tracks.length; i++) {
+
         tracks[0].stop();
     }
+
     videostream.srcObject = null;
 }
-function takeCardSC() {
+function takeCardSC(): void {
     if (image.src == '') {
-        context.drawImage(videostream, 0, 0, canvas.width, canvas.height);
+        context.drawImage(videostream, 0, 0, canvas.width, canvas.height)
         let url = canvas.toDataURL('image/jpeg', 1.0);
         image.src = url;
         console.log('about to call analyse');
+        
+
     }
     else {
         image.removeAttribute("src");
-        context.drawImage(videostream, 0, 0, canvas.width, canvas.height);
+        context.drawImage(videostream, 0, 0, canvas.width, canvas.height)
         let url = canvas.toDataURL('image/jpeg', 1.0);
         image.src = url;
         console.log('about to call analyse');
+        
     }
 }
-//# sourceMappingURL=camAccess.js.map
